2d23ee11bc19a2cdae918b441ea41cb675d390a5
Pariola <kehindephilip15@gmail.com>
2026-04-26 23:27:11 +0100

    chore: release v1.4.0 (dev + stable public launch)

--- Casks/zeity.rb
+++ Casks/zeity.rb
@@ -1,9 +1,9 @@
-cask "zeity-dev" do
+cask "zeity" do
   version "1.4.0"
   sha256 "9a709aa7f00b459d390908b2795c2e2b50853c7275acbd2ef35fd0ddfcf0dfae"
 
   url "https://github.com/Pariola-droid/homebrew-zeity/releases/download/v#{version}/Zeity-v#{version}.zip"
-  name "Zeity (Dev)"
+  name "Zeity"
   desc "Global timezone companion — track your friends' local times from the menu bar"
   homepage "https://github.com/Pariola-droid/zt-mac"
 
